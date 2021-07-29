8d1ce02c9a9cd16076324262b483b58a2c0baa8a
abdullahmert <abdbirisi77@gmail.com>
2021-07-29 05:33:03 +0000

    key region

--- key-pair/key-pair.tf
+++ key-pair/key-pair.tf
@@ -1,5 +1,7 @@
+
 resource "aws_key_pair" "ilearning-wordpress" {
   key_name   = "ilearning-wordpress"
   public_key = file("~/.ssh/id_rsa.pub")
+  region = "us-east-2"
 
 }
--- key-pair/output.tf
+++ key-pair/output.tf
@@ -5,3 +5,8 @@ output "key_name" {
 output "key_id" {
   value = aws_key_pair.ilearning-wordpress.id
 }
+
+output "region" {
+  value= aws_key_pair.ilearning-wordpress.region
+}
+
