2f15a89d2d3eb83702dcf435a92ff6d5ff8e607b
abdullahmert <abdbirisi77@gmail.com>
2021-08-05 22:30:29 +0000

    updated

---
+++ iam/iam.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_user" "bob" {
+  name = "bob"
+  path = "/system/"
+
+  tags = {
+    Team = "DevOps"
+  }
+}
+
+
+resource "aws_iam_group" "sysusers" {
+  name = "sysusers"
+  path = "/users/"
+}
+
+
+resource "aws_iam_user_group_membership" "BobSystem" {
+  user = aws_iam_user.bob.name
+
+  groups = [
+    aws_iam_group.sysusers.name,
+   
+  ]
+}
---
+++ iam/output.tf
@@ -0,0 +1,7 @@
+output "UserName" {
+    value= aws_iam_user.bob.name
+}
+
+output "GroupName" {
+    value= aws_iam_group.sysusers.name
+}
